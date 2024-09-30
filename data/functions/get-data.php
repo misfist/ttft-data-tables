1d78c597b257634f2f416c20bfb41759304ad584
Pea <pea@misfist.com>
2024-09-29 23:39:04 -0400

    Clean up

--- data/functions/get-data.php
+++ data/functions/get-data.php
@@ -84,11 +84,6 @@ function get_top_ten_raw_data( $donor_type = '', $donation_year = '', $number_of
  * @return array
  */
 function get_single_think_tank_raw_data( $think_tank = '', $donation_year = '', $donor_type = '' ): array {
-
-	$think_tank_var    = $_GET['think_tank'];
-	$donation_year_var = $_GET['donation_year'];
-	$donor_type_var    = $_GET['donor_type'];
-
 	$think_tank    = ( $think_tank_var ) ? sanitize_text_field( $think_tank_var ) : sanitize_text_field( $think_tank );
 	$donation_year = ( $donation_year_var ) ? sanitize_text_field( $donation_year_var ) : sanitize_text_field( $donation_year );
 	$donor_type    = ( $donor_type_var ) ? sanitize_text_field( $donor_type_var ) : sanitize_text_field( $donor_type );
