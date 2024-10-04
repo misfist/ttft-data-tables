44e26756cd45afceb0a9d62c66fd6a90978644ec
Pea <pea@misfist.com>
2024-10-03 21:42:40 -0400

    Helper functions
    Add transparency score-related helpers

--- data/functions/helpers.php
+++ data/functions/helpers.php
@@ -47,3 +47,60 @@ function get_post_from_term( $slug, $type ) {
 
 	return get_posts( $args );
 }
+
+/**
+ * Get Post Transparency Score
+ *
+ * @param  integer $post_id
+ * @return string
+ */
+function get_star_rating( $post_id = 0 ): string {
+	$post_id = (int) $post_id ?? get_the_ID();
+	$score   = get_post_meta( $post_id, 'transparency_score', true );
+
+	return convert_start_rating( $score );
+}
+
+/**
+ * Render Star Rating
+ *
+ * @param  integer $post_id
+ * @return void
+ */
+function render_star_rating( $post_id = 0 ): void {
+	echo get_star_rating( $post_id );
+}
+
+/**
+ * Convert the Transparency Score to a star rating.
+ *
+ * @param int $score The Transparency Score.
+ * @return string The star rating.
+ */
+function convert_start_rating( $score ): string {
+	$max = 5;
+	ob_start();
+	?>
+	<!-- wp:group {"metadata":{"name":"Transparency Stars"},"className":"stars-<?php echo $score; ?>","layout":{"type":"default"}} -->
+	<div class="wp-block-group stars-<?php echo $score; ?>" aria-label="<?php echo $score; ?> stars">
+		<?php
+		for ( $x = 1; $x <= $score && $x <= $max; $x++ ) :
+			?>
+			<span class="icon material-symbols-outlined rating-star" data-filled="true">star</span>
+			<?php
+		endfor;
+
+		for ( $x = ( $score + 1 ); $x <= $max; $x++ ) :
+			?>
+			<span class="icon material-symbols-outlined rating-star" data-filled="false">star_outline</span>
+			<?php
+		endfor;
+		?>
+	</div>
+	<!-- /wp:group -->
+
+	<?php
+	$stars = ob_get_clean();
+	return $stars;
+}
+
