38a329deb40fec2f124ca187e712c73a3a9be290
Pea <pea@misfist.com>
2024-12-13 00:34:06 -0500

    Remove source column
    Remove source column because it applies to a specific transaction, but our rows are aggregated.

--- src/class-render.php
+++ src/class-render.php
@@ -266,7 +266,6 @@ class Render {
 					<tr>
 						<th class="column-donor" scope="col"><?php esc_html_e( 'Donor', 'data-tables' ); ?></th>
 						<th class="column-numeric column-min-amount" scope="col"><?php esc_html_e( 'Min Amount', 'data-tables' ); ?></th>
-						<th class="column-source" scope="col"><?php esc_html_e( 'Source', 'data-tables' ); ?></th>
 						<th class="column-type" scope="col"><?php esc_html_e( 'Type', 'data-tables' ); ?></th>
 					</tr>
 				</thead>
@@ -288,7 +287,6 @@ class Render {
 							<td class="column-numeric column-min-amount" data-heading="<?php esc_attr_e( 'Min. Amount', 'data-tables' ); ?>" data-order="<?php echo esc_attr( $attrs['sort'] ); ?>>">
                                 <span class="<?php echo $attrs['class']; ?>" data-label="<?php echo $attrs['label']; ?>"><span><?php echo esc_html( number_format( $amount, 0, '.', ',' ) ); ?></span></span>
 							</td>
-							<td class="column-source"><?php echo ( $row['source'] ) ? sprintf( '<a href="%1$s" aria-label="%2$s" target="_blank"><span class="material-symbols-outlined" style="font-family:var(--wp--preset--font-family--icon);" role="img" aria-label="%2$s">link</span></a>', esc_url( $row['source'] ), esc_attr__( 'Link to source', 'data-tables' ) ) : ''; ?></td>
 							<td class="column-donor-type" data-heading="<?php esc_attr_e( 'Type', 'data-tables' ); ?>"><?php echo $row['donor_type']; ?></td>
 						</tr>
 					<?php endforeach; ?>
@@ -385,7 +383,6 @@ class Render {
 						<th class="column-think-tank" scope="col"><?php esc_html_e( 'Think Tank', 'data-tables' ); ?></th>
 						<th class="column-donor" scope="col"><?php esc_html_e( 'Donor', 'data-tables' ); ?></th>
 						<th class="column-numeric column-min-amount" scope="col"><?php esc_html_e( 'Min Amount', 'data-tables' ); ?></th>
-						<th class="column-source" scope="col"><?php esc_html_e( 'Source', 'data-tables' ); ?></th>
 					</tr>
 				</thead>
 				<tbody
@@ -406,7 +403,6 @@ class Render {
 							<td class="column-numeric column-min-amount" data-heading="<?php esc_attr_e( 'Min. Amount', 'data-tables' ); ?>" data-order="<?php echo esc_attr( $attrs['sort'] ); ?>">
                                 <span class="<?php echo $attrs['class']; ?>" data-label="<?php echo $attrs['label']; ?>"><span><?php echo esc_html( number_format( $amount, 0, '.', ',' ) ); ?></span></span>
 							</td>
-							<td class="column-source" data-heading="<?php esc_attr_e( 'Source', 'data-tables' ); ?>"><?php echo ( $row['source'] ) ? sprintf( '<a href="%1$s" aria-label="%2$s" target="_blank"><span class="material-symbols-outlined" style="font-family:var(--wp--preset--font-family--icon);" role="img" aria-label="%2$s">link</span></a>', esc_url( $row['source'] ), esc_attr__( 'Link to source', 'data-tables' ) ) : ''; ?></td>
 						</tr>
 						
 					<?php endforeach; ?>
