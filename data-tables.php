9703c00f68c1c50b6a85e2f745cc89bd95e9243f
Pea <pea@misfist.com>
2024-12-14 00:49:18 -0500

    Update plugin header

--- data-tables.php
+++ data-tables.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Data Tables - Think Tank Funding Tracker
  * Description:       Display data tables.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Author:            Pea
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-const VERSION       = '1.0.0';
+const VERSION       = '1.0.1';
 const APP_NAMESPACE = 'ttft/data-tables';
 
 $autoload = __DIR__ . '/vendor/autoload.php';
